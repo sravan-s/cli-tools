35f77b03986f0f573ad42a80ee7f3a3387827a94
sravan <sravan-s@protonmail.com>
2024-03-31 18:02:51 +0900

    In progress - mkdir
    Trick part - canonizing paths

--- mkdir/specs.md
+++ mkdir/specs.md
@@ -11,7 +11,7 @@ DESCRIPTION
        tions too.
 
        -m=MODE
-              set file mode (as in chmod), not a=rwx - umask
+    set file mode (as in chmod), not a=rwx - umask
 
        -p
               no error if existing, make parent directories as  needed,
@@ -20,15 +20,18 @@ DESCRIPTION
        -v
               print a message for each created directory
 
-       -Z     set SELinux security context of each created directory to
-              the default type
-
-       --context[=CTX]
-              like -Z, or if CTX is specified then set the  SELinux  or
-              SMACK security context to CTX
-
        --help display this help and exit
 
        --version
               output version information and exit
+
+
+> About path, we handle the follwoing special cases:
+
+    .. (dot-dot): Represents the parent directory.
+    ~ (tilde): Represents the user's home directory.
+    / (root): Represents the root directory.
+    . (dot): Represents the current directory.
+    (name) -> same as dot
+    example: mkdir ./new_dir = mkdir new_dir 
 
--- mkdir/src/main.rs
+++ mkdir/src/main.rs
@@ -1,22 +1,141 @@
-use std::env;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
+use std::{env, process::exit};
 
 const VERSION: &str = "0.0.1";
+const EXIT_SUCCESS: i32 = 0;
+const EXIT_FAILURE: i32 = 1;
+const EXIT_FAIL_PERMISSION: i32 = 2;
 
-struct Args {
-    chmod: String,
-    path: String,
-    make_parent: bool,
-    context: String,
+struct Options<'a> {
+    make_parents: bool,
+    mode: u32,
+    verbose: bool,
+    path: Option<&'a str>,
+}
+
+/*
+fn _canonicalize_path(p: &PathBuf, pwd: &PathBuf) -> PathBuf {
+    let start_with_dot =
+}
+*/
+
+fn make(options: Options) {
+    let _pwd = match env::current_dir() {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("Couldnt find current path {}", e);
+            exit(EXIT_FAILURE)
+        }
+    };
+
+    let path_to_create = match options.path {
+        Some(p) => p,
+        None => {
+            eprintln!("No directory to create, input a path mkdir ./my_dir");
+            exit(EXIT_FAILURE)
+        }
+    };
+
+    if options.make_parents {
+        println!("to implement");
+    } else {
+        /*
+        let to_canon = path_to_create.to_string() + "/..";
+        let path = match fs::canonicalize(to_canon.clone()) {
+            Ok(p) => p,
+            Err(e) => {
+                eprintln!("Couldnt canonicalize the path {}", e);
+                dbg!(to_canon);
+                exit(EXIT_FAILURE)
+            }
+        };
+        */
+        let created = match fs::create_dir("./") {
+            Ok(_c) => fs::canonicalize(path_to_create).unwrap(),
+            Err(e) => {
+                eprintln!("Couldnt craete directory {}", e);
+                exit(EXIT_FAILURE)
+            }
+        };
+        let mut permissions = fs::metadata(created.clone())
+            .expect("Failed to get metadata")
+            .permissions();
+        permissions.set_mode(options.mode);
+
+        match fs::set_permissions(created.clone(), permissions) {
+            Ok(_) => {
+                println!("Directory created successfully.");
+                dbg!(created);
+                exit(EXIT_SUCCESS)
+            }
+            Err(e) => {
+                eprintln!("Couldnt set permissions to directory {}", e);
+                exit(EXIT_FAIL_PERMISSION)
+            }
+        }
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let specs = include_bytes!("../specs.md");
-    if args.iter().any(|i| i == "--version") {
-        print!("version: {}", VERSION);
+    let mut params = Options {
+        make_parents: false,
+        mode: 0o755,
+        verbose: false,
+        path: None,
+    };
+    /* first arg is usually binary path */
+    let mut i = 1;
+    while i < args.len() {
+        if args[i] == "--version" {
+            print!("version: {}\n", VERSION);
+            exit(EXIT_SUCCESS)
+        }
+
+        if args[i] == "--help" {
+            let specs = include_bytes!("../specs.md");
+            print!("{}\n", String::from_utf8_lossy(specs));
+            exit(EXIT_SUCCESS)
+        }
+
+        if args[i] == "-p" {
+            params.make_parents = true;
+            i += 1;
+            continue;
+        }
+
+        if args[i] == "-v" {
+            params.verbose = true;
+            i += 1;
+            continue;
+        }
+
+        if args[i] == "-m" {
+            let mode: u32 = match args[i + 1].parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    eprintln!("Failed to parse permission string: '{}'", args[i + 1]);
+                    exit(EXIT_FAILURE);
+                }
+            };
+            params.mode = mode;
+            i = i + 2;
+            continue;
+        }
+
+        match params.path {
+            Some(_) => {
+                eprintln!("Unknown paramter {}", args[i]);
+                exit(EXIT_FAILURE);
+            }
+            None => {
+                params.path = Some(&args[i]);
+                i += 1;
+            }
+        }
     }
 
-    if args.iter().any(|i| i == "--help") {
-        print!("{}", String::from_utf8_lossy(specs));
-    }
+    make(params);
 }
