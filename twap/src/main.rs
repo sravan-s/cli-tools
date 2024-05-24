e146c7bd639e0f1baafe0171fa7a887499a747ed
sravan <sravan-s@protonmail.com>
2024-05-24 17:55:36 +0900

    Implement basic view of process

--- twap/src/main.rs
+++ twap/src/main.rs
@@ -1,3 +1,33 @@
+use sysinfo::System;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    println!("=> system:");
+    // RAM and swap information:
+    println!("total memory: {} bytes", sys.total_memory());
+    println!("used memory : {} bytes", sys.used_memory());
+    println!("total swap  : {} bytes", sys.total_swap());
+    println!("used swap   : {} bytes", sys.used_swap());
+
+    // Display system information:
+    println!("System name:             {:?}", System::name());
+    println!("System kernel version:   {:?}", System::kernel_version());
+    println!("System OS version:       {:?}", System::os_version());
+    println!("System host name:        {:?}", System::host_name());
+
+    // Number of CPUs:
+    println!("NB CPUs: {}", sys.cpus().len());
+
+    // Display processes ID, name na disk usage:
+    for (pid, process) in sys.processes() {
+        println!(
+            "[{:>20}] {:<40} {:>20} {:>20}",
+            pid,
+            process.name(),
+            process.memory(),
+            process.cpu_usage()
+        );
+    }
 }
